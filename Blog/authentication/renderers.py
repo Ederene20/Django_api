485a9c57749c6bad28a562c5d74b25a343fa15a7
Ederene20 <atinhoruben@gmail.com>
2021-02-24 10:34:46 +0100

    Modify renderers.py

--- Blog/authentication/renderers.py
+++ Blog/authentication/renderers.py
@@ -3,4 +3,11 @@ from rest_framework.renderers import JSONRenderer
 
 class UserJSONRenderer(JSONRenderer):
     charset = 'utf-8'
+
+    def render(self, data, media_type = None, renderer_context = None):
+
+        token = data.get('token', None)
+
+        if token is not None and isinstance(token, bytes):
+            
     
